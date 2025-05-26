151e0e7861bc4c858ac64a37af626dcbe1c71e83
lifpro <ysalissou@gmail.com>
2025-05-26 18:56:14 +0000

    GridView

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:cake_client_app/data/cakes_data.dart';
-import 'package:cake_client_app/utils/contantes.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -35,31 +34,53 @@ class _HomePageState extends State<HomePage> {
           actions: [
             Icon(Icons.settings),
           ]),
-      body: ListView.separated(
-          // itemBuilder: (context, index) => CakeCard(
-          //     cake: cakes[index],
-          itemBuilder: (context, index) => ListTile(
-                leading: CircleAvatar(
-                  backgroundImage:
-                      AssetImage('assets/images/${cakes[index].image}'),
-                  radius: 50,
-                ),
-                title: Text(
-                  cakes[index].name,
-                  style: TextStyle(fontWeight: FontWeight.w900),
-                ),
-                subtitle: Text('Disponible'),
-                trailing: CircleAvatar(
-                  foregroundColor: Colors.white,
-                  backgroundColor: myPrimatyColor,
-                  child: Text('17'),
-                ),
-                contentPadding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-              ),
-          separatorBuilder: (context, index) => SizedBox(
-                height: 5,
-              ),
-          itemCount: cakes.length),
+      body: Container(
+        child: GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          children: cakes
+              .map((c) => Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.all(5),
+                    child: Image.asset(
+                      'assets/images/${c.image}',
+                      fit: BoxFit.cover,
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
+      // body: ListView.separated(
+      //     // itemBuilder: (context, index) => CakeCard(
+      //     //     cake: cakes[index],
+      //     itemBuilder: (context, index) => ListTile(
+      //           leading: CircleAvatar(
+      //             backgroundImage:
+      //                 AssetImage('assets/images/${cakes[index].image}'),
+      //           ),
+      //           title: Text(
+      //             cakes[index].name,
+      //             style: TextStyle(fontWeight: FontWeight.w900),
+      //           ),
+      //           subtitle: Text('Disponible'),
+      //           // trailing: CircleAvatar(
+      //           //   foregroundColor: Colors.white,
+      //           //   backgroundColor: myPrimatyColor,
+      //           //   child: Text('17'),
+      //           // ),
+      //           trailing: Checkbox(
+      //             tristate: true,
+      //             activeColor: myPrimatyColor,
+      //             checkColor: Colors.white,
+      //             onChanged: (bool? value) => switchChecked(cakes[index]),
+      //             value: cakes[index].checked,
+      //           ),
+      //           contentPadding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+      //         ),
+      //     separatorBuilder: (context, index) => SizedBox(
+      //           height: 5,
+      //         ),
+      //     itemCount: cakes.length),
       // body: ListView(children: [
       //   CakeCard(
       //       name: 'Gateau 1',
