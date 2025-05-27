2de243b0c57614d60ec7fa3487e997f48a7431ab
lifpro <ysalissou@gmail.com>
2025-05-27 18:01:26 +0000

    data

--- lib/data/cakes_data.dart
+++ lib/data/cakes_data.dart
@@ -1,13 +1,58 @@
 import 'package:cake_client_app/models/cake_model.dart';
 
 List<CakeModel> cakes = [
-  CakeModel(name: 'Fôret noire', image: '1.png', checked: false),
-  CakeModel(name: 'Fôret blanche', image: '2.png', checked: false),
-  CakeModel(name: 'Fôret melange', image: '3.png', checked: false),
-  CakeModel(name: 'Fôret Violet', image: '1.png', checked: false),
-  CakeModel(name: 'Fôret rose', image: '2.png', checked: false),
-  CakeModel(name: 'Fôret marron', image: '3.png', checked: false),
-  CakeModel(name: 'Fôret Violet', image: '1.png', checked: false),
-  CakeModel(name: 'Fôret rose', image: '2.png', checked: false),
-  CakeModel(name: 'Fôret marron', image: '3.png', checked: false),
+  CakeModel(
+      name: 'Fôret noire',
+      image: '1.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret blanche',
+      image: '2.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret melange',
+      image: '3.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret Violet',
+      image: '1.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret rose',
+      image: '2.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret marron',
+      image: '3.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret Violet',
+      image: '1.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret rose',
+      image: '2.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
+  CakeModel(
+      name: 'Fôret marron',
+      image: '3.png',
+      description:
+          'La forêt-noire, en allemand Schwarzwälder Kirschtorte, littéralement « gâteau à la cerise de la Forêt-Noire », est une pâtisserie allemande constituée de génoise au cacao imbibée de kirsch puis fourrée de crème chantilly et de cerises. Elle est recouverte de crème chantilly et décorée de copeaux de chocolat.',
+      checked: false),
 ];
--- lib/models/cake_model.dart
+++ lib/models/cake_model.dart
@@ -2,6 +2,11 @@ class CakeModel {
   String? id;
   String name;
   String image;
+  String description;
   bool checked;
-  CakeModel({required this.name, required this.image, required this.checked});
+  CakeModel(
+      {required this.name,
+      required this.image,
+      required this.description,
+      required this.checked});
 }
