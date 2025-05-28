22e4d315d162648ed423e92dba1a23af0125991a
lifpro <ysalissou@gmail.com>
2025-05-28 18:17:08 +0000

    Les formulaires

--- lib/home/home_profil.dart
+++ lib/home/home_profil.dart
@@ -10,6 +10,21 @@ class HomeProfilPage extends StatefulWidget {
 
 class _HomeProfilPageState extends State<HomeProfilPage> {
   final _formKey = GlobalKey<FormState>();
+  late String nom = "";
+  late String email = "";
+  late int numero;
+
+  void connexion() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print("nom=" + nom);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Connexion effectuée aves success'),
+        duration: Durations.extralong4,
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -30,11 +45,21 @@ class _HomeProfilPageState extends State<HomeProfilPage> {
                           fontSize: 18,
                           color: myPrimatyColor),
                     )),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Le nom est requis';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  nom = newValue!;
+                },
               ),
               SizedBox(
                 height: 10,
               ),
               TextFormField(
+                keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                     hintText: "Veuillez saisir votre email",
                     label: Text(
@@ -44,17 +69,47 @@ class _HomeProfilPageState extends State<HomeProfilPage> {
                           fontSize: 18,
                           color: myPrimatyColor),
                     )),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "L'émail est requis";
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  email = newValue!;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                    hintText: "Veuillez saisir votre numéro",
+                    label: Text(
+                      "Votre numéro",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 18,
+                          color: myPrimatyColor),
+                    )),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Le numéro est requis";
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  numero = int.parse(newValue!);
+                },
               ),
               SizedBox(
                 height: 20,
               ),
               ElevatedButton(
-                onPressed: () => {},
-                child: Text('Se connecter'),
+                onPressed: () => connexion(),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: myPrimatyColor, // Couleur de fond
                   foregroundColor: Colors.white, // Couleur du texte/icone
                 ),
+                child: Text('Se connecter'),
               )
             ],
           ),
